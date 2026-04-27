9815b77aa6e2a1517001a96941483055efdb9a68
AshaJenvasu <chaiyawatjenvasu@gmail.com>
2026-04-27 09:25:49 +0700

    Add: move Navbar and Footer to App.jsx as a Global State

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import { Route, Routes } from "react-router";
 import Home from "./pages/Home";
+import Navbar from "./components/Home/00_Navbar";
+import Footer from "./components/Home/08_Footer";
+
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <Navbar />
+
+      <div className="min-h-screen">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+
+      <Footer />
     </>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,19 @@
-import Navbar from "../components/Home/00_Navbar";
 import Hero from "../components/Home/01_Hero";
 import ScrollingText from "../components/Home/02_ScrollingText";
 import Highlight from "../components/Home/03_Highlight";
 import Market from "../components/Home/04_Market";
 import ArtistLove from "../components/Home/05_Artist-love";
 import AboutUs from "../components/Home/07_AboutUs";
-import Footer from "../components/Home/08_Footer";
 
 const Home = () => {
   return (
     <main>
-      <Navbar />
       <Hero />
       <ScrollingText />
       <Highlight />
       <Market />
       <ArtistLove />
       <AboutUs />
-      <Footer />
     </main>
   );
 };
