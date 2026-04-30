f331542725810b7f747cae1db393c4257ab6bfb7
Wathisa <wathisa@gmail.com>
2026-04-30 11:29:13 +0700

    feat: add ArtistLinks component

--- src/components/ArtistProfile/04_ArtistLinks.jsx
+++ src/components/ArtistProfile/04_ArtistLinks.jsx
@@ -1,5 +1,23 @@
 const ArtistLinks = () => {
-  return <section></section>;
+  return (
+    <section className="w-full">
+      <div className="flex max-w-xl flex-col gap-4">
+        <a
+          href="#"
+          className="inline-flex w-fit min-w-64 items-center justify-center rounded-xl bg-black px-6 py-3 text-lg font-medium text-white"
+        >
+          Instragram/Starchaser.dev
+        </a>
+
+        <a
+          href="#"
+          className="inline-flex w-fit min-w-72 items-center justify-center rounded-xl bg-black px-6 py-3 text-lg font-medium text-white"
+        >
+          Instagram/Starchaser.playground
+        </a>
+      </div>
+    </section>
+  );
 };
 
 export default ArtistLinks;
