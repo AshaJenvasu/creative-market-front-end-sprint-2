ce0be13c984d74d919d5ff4b81e8f7f811bdc260
Wathisa <wathisa@gmail.com>
2026-04-30 11:40:12 +0700

    feat: add ArtistProductGrid component

--- src/components/ArtistProfile/06_ArtistProductGrid.jsx
+++ src/components/ArtistProfile/06_ArtistProductGrid.jsx
@@ -2,8 +2,18 @@ import ArtistProductCard from "./07_ArtistProductCard";
 
 const ArtistProductGrid = () => {
   return (
-    <section>
-      <ArtistProductCard />
+    <section className="w-full">
+      <div className="grid grid-cols-1 gap-8 md:grid-cols-2 xl:grid-cols-3">
+        <ArtistProductCard />
+        <ArtistProductCard />
+        <ArtistProductCard />
+        <ArtistProductCard />
+        <ArtistProductCard />
+        <ArtistProductCard />
+        <ArtistProductCard />
+        <ArtistProductCard />
+        <ArtistProductCard />
+      </div>
     </section>
   );
 };
